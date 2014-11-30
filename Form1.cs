ec66bf8b2ed837cc111ed6c7fda533b78b4fc59c
Alex Wong <alexwong@bu.edu>
2014-11-29 23:45:56 -0500

    Finish exception handling, program now tests to specifications

--- Form1.cs
+++ Form1.cs
@@ -67,49 +67,58 @@ namespace file_encypt_decrypt
             {
                 // Generate variables needed to encrypt from user entry
                 string inName = this.FilePath;
-                string outName = this.FilePath + ".des";
+                string outName = this.FilePath + ".des";    // Add .des to
                 if (this.KeyTextBox.Text == "")   // Check for empty key
                 {
                     throw new Exception("Please enter a key.");
+                }
+                if (!overwriteifExist(outName)) // Check to see if the file already exists and if user doesn't want to overwrite
+                {
+                    throw new IOException("File not overwritten.");
+                    // Else continue and overwrite
                 }
                 byte[] desKey = this.keytoByteArray();
                 byte[] desIV = this.keytoByteArray();
 
                 //Create the file streams to handle the input and output files.
-                FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
-                FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
-                fout.SetLength(0);
-
-                //Create variables to help with read and write. 
-                byte[] bin = new byte[100];     //This is intermediate storage for the encryption. 
-                long rdlen = 0;                 //This is the total number of bytes written. 
-                long totlen = fin.Length;       //This is the total length of the input file. 
-                int len;                        //This is the number of bytes to be written at a time.
-
-                DES des = new DESCryptoServiceProvider();
-                CryptoStream encStream = new CryptoStream(fout, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
-
-                Console.WriteLine("Encrypting...");
-
-                //Read from the input file, then encrypt and write to the output file. 
-                while (rdlen < totlen)
+                using (FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read))
+                using (FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write))
                 {
-                    len = fin.Read(bin, 0, 100);
-                    encStream.Write(bin, 0, len);
-                    rdlen = rdlen + len;
-                    Console.WriteLine("{0} bytes processed", rdlen);
-                }
+                    // using block makes sure filestreams are closed if an exception occurs
 
-                Console.WriteLine("Encryption complete.");
-                encStream.Close();
-                fout.Close();
-                fin.Close(); 
+                    fout.SetLength(0);
+
+                    //Create variables to help with read and write. 
+                    byte[] bin = new byte[100];     //This is intermediate storage for the encryption. 
+                    long rdlen = 0;                 //This is the total number of bytes written. 
+                    long totlen = fin.Length;       //This is the total length of the input file. 
+                    int len;                        //This is the number of bytes to be written at a time.
+
+                    DES des = new DESCryptoServiceProvider();
+                    CryptoStream encStream = new CryptoStream(fout, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+
+                    Console.WriteLine("Encrypting...");
+
+                    //Read from the input file, then encrypt and write to the output file. 
+                    while (rdlen < totlen)
+                    {
+                        len = fin.Read(bin, 0, 100);
+                        encStream.Write(bin, 0, len);
+                        rdlen = rdlen + len;
+                        Console.WriteLine("{0} bytes processed", rdlen);
+                    }
+
+                    Console.WriteLine("Encryption complete.");
+                    encStream.Close();
+                    fout.Close();
+                    fin.Close(); 
+                }
             }
             catch (Exception e)
             {
                 if (e is System.IO.FileNotFoundException)
                 {
-                    MessageBox.Show("Could not open source or destination file",
+                    MessageBox.Show("Could not open source or destination file.",
                         "Error",
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Error,
@@ -122,6 +131,10 @@ namespace file_encypt_decrypt
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Error,
                         MessageBoxDefaultButton.Button1);
+                }
+                else if (e is IOException) // No file overwrite
+                {
+                    // Do nothing
                 }
                 else // Manually thrown exception or exception not explicitly handled
                 {
@@ -140,53 +153,63 @@ namespace file_encypt_decrypt
             {
                 // Generate variables needed to encrypt from user entry
                 string inName = this.FilePath;
-                if (Path.GetExtension(inName) != ".des")
-                {
-                    throw new Exception("Not a .des file.");
-                }
-                string outName = Path.GetFileNameWithoutExtension(FilePath);  // Remove ".des" extension
                 if (this.KeyTextBox.Text == "") // Check for empty key
                 {
                     throw new Exception("Please enter a key.");
                 }
+                if (Path.GetExtension(inName) != ".des")
+                {
+                    throw new Exception("Not a .des file.");
+                }
+                string outName = Path.ChangeExtension(FilePath, "");  // Remove ".des" extension
+                if (!overwriteifExist(outName)) // Check to see if the file already exists and if user doesn't want to overwrite
+                {
+                    throw new IOException("File not overwritten");
+                    // Else continue and overwrite
+                }
+                
                 byte[] desKey = this.keytoByteArray();
                 byte[] desIV = this.keytoByteArray();
 
                 //Create the file streams to handle the input and output files.
-                FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
-                FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
-                fout.SetLength(0);
-
-                //Create variables to help with read and write. 
-                byte[] bin = new byte[100];     //This is intermediate storage for the encryption. 
-                long rdlen = 0;                 //This is the total number of bytes written. 
-                long totlen = fin.Length;       //This is the total length of the input file. 
-                int len;                        //This is the number of bytes to be written at a time.
-
-                DES des = new DESCryptoServiceProvider();
-                CryptoStream decStream = new CryptoStream(fout, des.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
-
-                Console.WriteLine("Decrypting...");
-
-                //Read from the input file, then encrypt and write to the output file. 
-                while (rdlen < totlen)
+                using (FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read))
+                using (FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write))
                 {
-                    len = fin.Read(bin, 0, 100);
-                    decStream.Write(bin, 0, len);
-                    rdlen = rdlen + len;
-                    Console.WriteLine("{0} bytes processed", rdlen);
-                }
+                    // using block makes sure filestreams are closed if an exception occurs
 
-                Console.WriteLine("Decryption complete.");
-                decStream.Close();
-                fout.Close();
-                fin.Close();   
+                    fout.SetLength(0);
+
+                    //Create variables to help with read and write. 
+                    byte[] bin = new byte[100];     //This is intermediate storage for the encryption. 
+                    long rdlen = 0;                 //This is the total number of bytes written. 
+                    long totlen = fin.Length;       //This is the total length of the input file. 
+                    int len;                        //This is the number of bytes to be written at a time.
+
+                    DES des = new DESCryptoServiceProvider();
+                    CryptoStream decStream = new CryptoStream(fout, des.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
+
+                    Console.WriteLine("Decrypting...");
+
+                    //Read from the input file, then encrypt and write to the output file. 
+                    while (rdlen < totlen)
+                    {
+                        len = fin.Read(bin, 0, 100);
+                        decStream.Write(bin, 0, len);
+                        rdlen = rdlen + len;
+                        Console.WriteLine("{0} bytes processed", rdlen);
+                    }
+
+                    Console.WriteLine("Decryption complete.");
+                    decStream.Close();
+                    fout.Close();
+                    fin.Close();
+                }
             }
             catch (Exception e)
             {
                 if (e is System.IO.FileNotFoundException)
                 {
-                    MessageBox.Show("Could not open source or destination file",
+                    MessageBox.Show("Could not open source or destination file.",
                         "Error",
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Error,
@@ -199,6 +222,13 @@ namespace file_encypt_decrypt
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Error,
                         MessageBoxDefaultButton.Button1);
+                    
+                    // Clean up file stream/created file
+                    File.Delete(Path.ChangeExtension(FilePath, ""));
+                }
+                else if (e is IOException)  // No file overwrite
+                {
+                    // Do nothing
                 }
                 else // Manually thrown exception or exception not explicitly handled
                 {
@@ -234,6 +264,22 @@ namespace file_encypt_decrypt
             }
 
             return KeyArray;
+        }
+
+        public bool overwriteifExist(string outName)
+        {
+            if (File.Exists(outName))
+            {
+                var result = MessageBox.Show("Output file exists. Overwrite?",
+                "Error",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+
+                if (result == DialogResult.Yes) return true;
+                else return false;
+            }
+            return true;
         }
     }
 }
